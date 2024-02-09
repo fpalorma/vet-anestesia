aec48a54fef161d51470ef0c0b7aea0d9b627b67
Agustin Labiano <agustinlabiano@hotmail.com>
2024-02-09 00:17:59 +0000

    feat: print budget in pdf

--- src/app/components/budget/index.jsx
+++ src/app/components/budget/index.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { jsPDF } from "jspdf";
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
@@ -20,10 +21,25 @@ const AsaSecondaryText = ({ value }) => (
 
 const Budget = ({ asa, drugs = [], disabled, weight }) => {
   const [budget, setBudget] = useState(0);
+  const ref = useRef(null);
 
   const handleOnQuote = () => {
     const total = getBudget(asa, weight, drugs);
     setBudget(total);
+  };
+
+  const exportPdf = () => {
+    const doc = new jsPDF();
+    const { current } = ref;
+
+    doc.html(current, {
+      callback: function (doc) {
+        doc.save('Presupuesto.pdf'); // TODO: add date to the name of the file
+      },
+      margin: [0, 30, 0, 30],
+      width: 150,
+      windowWidth: current.clientWidth
+    });
   };
 
   /* This hides the budget if user changes the list of drugs or the asa */
@@ -42,9 +58,20 @@ const Budget = ({ asa, drugs = [], disabled, weight }) => {
       >
         Presupuestar
       </Button>
+      {/* TODO: add margin top and disabled behavior when there is no budget and change button icon */}
+      <Button
+        fullWidth 
+        variant="contained"
+        onClick={exportPdf}
+        startIcon={<ReceiptOutlinedIcon />}
+        disabled={disabled}
+      >
+        Exportar
+      </Button>
       {
-        !!budget && <Box sx={{ mt: 2 }}>
+        !!budget && <Box sx={{ mt: 2 }} ref={ref}>
           <Paper elevation={3} sx={{ p: 2, bgcolor: 'primary.main' }}>
+            {/* TODO: check font family, should be Roboto */}
             <Box className={styles.title}>Presupuesto</Box>
             <List dense={true}>
               {
