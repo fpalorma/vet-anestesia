c72939e4efeda40fd0412c211ebf7fb3b8fd8acd
Agustin <agustinlabiano@hotmail.com>
2024-03-07 00:05:03 -0300

    feat: support for pwa (#15)

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,9 +9,30 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Anestesia',
-  description: 'Calculadora de costos de anestesia veterinaria',
-}
+  title: "VET Anestesia",
+  description: "Calculadora de costos de anestesia veterinaria",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["vet", "anestesia", "veterinaria", "calculadora", "calc"],
+  authors: [
+    { 
+      name: "Agustin Labiano", 
+      url: "https://ar.linkedin.com/in/agustin-labiano" 
+    },
+    {
+      name: "Federico Palorma",
+      url: "https://ar.linkedin.com/in/federico-palorma-6a693720b",
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-192x192.png" },
+    { rel: "icon", url: "icons/icon-192x192.png" },
+  ],
+};
+
+export const viewport = {
+  themeColor:  { media: "(prefers-color-scheme: light)", color: "#ffffff" }
+};
 
 export default function RootLayout({ children }) {
   return (
