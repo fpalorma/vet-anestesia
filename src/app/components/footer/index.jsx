f927fdc487701f614f6f53d57397ef30652be9a9
Agustin Labiano <agustinlabiano@hotmail.com>
2024-05-23 03:18:08 +0000

    fix: styles

--- src/app/components/footer/index.jsx
+++ src/app/components/footer/index.jsx
@@ -3,11 +3,13 @@ import styles from './style.module.css';
 
 const Footer = ({ children }) => {
   return (
-    <footer className={styles.footer}>
-      <Box sx={{ borderTop: 2, borderColor: 'primary.main', p: '0.5rem' }}>
-        {children}
-      </Box>
-    </footer>
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <Box sx={{ borderTop: 2, borderColor: 'primary.main', p: '0.5rem' }}>
+          {children}
+        </Box>
+      </footer>
+    </div>
   );
 };
 
--- src/app/components/nav-bar/index.jsx
+++ src/app/components/nav-bar/index.jsx
@@ -37,7 +37,7 @@ const NavBar = (props) => {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex'  }}>
+    <Box sx={{ display: 'flex', paddingBottom: 8, zIndex: 1000 }}>
       <CssBaseline />
       <AppBar position="fixed">
         <Toolbar
